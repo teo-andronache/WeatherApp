7ac061bddbdc0cc487c49674df14d6d7ea3fd746
axandrei12 <andrei_axente@yahoo.com>
2023-07-09 15:12:49 +0300

    Implemented the SwichButton functionality(can switch between C and F)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import { WiDaySunny, WiThunderstorm, WiCloudy, WiRain, WiSnow, WiWindy } from 'react-icons/wi';
+import { ImSun, ImCloud } from 'react-icons/im';
 import videoBackground from './assets/video3.mp4';
 import './App.css';
 import SearchBar from './components/SearchBar';
-import SimpleSwitchButton from './components/SwitchButton';
-
+import SwitchSelector from "react-switch-selector";
+import TempButton from './components/SwitchButton';
 
 type WeatherData = {
   location: {
@@ -63,46 +63,32 @@ type SearchResult = {
   windKPH: number;
   humidity: number;
   condition: string;
-  localtime: string;
+
 };
 
 function App() {
-
+  let currentTemperature: number;
+  const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
-
-  const getIconByCondition = (condition: string) => {
-    if (condition.includes("rain") || condition.includes("Rain") || condition.includes("drizzle")) {
-      return <WiRain className='rain-icon' />;
-    } else if (condition.includes("Sun") || condition.includes("Clear")) {
-      return <WiDaySunny className='sun-icon' />;
-    } else if (condition.includes("Snow") || condition.includes("snow")) {
-      return <WiSnow className='snow-icon' />;
-    } else if (condition.includes("Wind") || condition.includes("wind")) {
-      return <WiWindy className='windy-icon' />;
-    } else if (condition.includes("Thunder") || condition.includes("thunder")) {
-      return <WiThunderstorm className='thunderstorm-icon' />;
-    } else if (condition.includes("Cloud") || condition.includes("cloud") || condition.includes("Overcast")) {
-      return <WiCloudy className='cloudy-icon' />;
-    }
-
-  };
-
-  /* Handling the searching an extracting the meaningful info. */
+  const [selectedUnit, setSelectedUnit] = useState("celsius");
+  
   const handleSearch = async (searchTerm: string) => {
-
     try {
       const apiKey = "17b74a5a5e844ac0b25133450230307";
       const response = await fetch(
         `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${searchTerm}&aqi=yes`
       );
-
+      
       const data: WeatherData = await response.json();
-      const currentTemperature = data.current.temp_c;
+      const currentTemperatureC = data.current.temp_c;
+      const currentTemperatureF = data.current.temp_f;
       const currentName = data.location.name;
       const currentWindKPH = data.current.wind_kph;
       const currentHumidity = data.current.humidity;
       const currentCondition = data.current.condition.text;
-      const currentLocaltime = data.location.localtime;
+
+      const currentTemperature = selectedUnit === "celsius" ? currentTemperatureC : currentTemperatureF;
+
 
 
       const searchResult: SearchResult = {
@@ -111,36 +97,35 @@ function App() {
         windKPH: currentWindKPH,
         humidity: currentHumidity,
         condition: currentCondition,
-        localtime: currentLocaltime
       };
 
       setSearchResults([searchResult]);
-
-
     } catch (error) {
       console.error("Error fetching weather data:", error);
-
       // Handle the error (e.g., show an error message)
     }
   };
-
+  const handleUnitChange = (unit: string) => {
+    setSelectedUnit(unit);
+    handleSearch(searchTerm); 
+  };
+  
   return (
     <div className='App'>
       <div className='search-bar-container'>
-        <SearchBar onSearch={handleSearch} />
-        <SimpleSwitchButton />
+        <SearchBar onSearch={handleSearch} /> 
+        <TempButton onUnitChange={handleUnitChange}/>
       </div>
+      
       <video src={videoBackground} autoPlay loop muted />
+      
       <div className='search-results-container'>
         {searchResults.map((result, index) => (
           <div key={index} className='search-result'>
-            <div className='result-name'>{result.name}
-              {" - " + result.localtime.slice(11, 16)}
-            </div>
-            <div className='result-temperature'>{result.temperature}°C</div>
-            <div className='result-condition'>
+            <div className='result-name'>{result.name}</div>
+            <div className='result-temperature'>{result.temperature}°{selectedUnit === "celsius" ? "C" : "F"}</div>            <div className='result-condition'>
               {result.condition}
-              {getIconByCondition(result.condition)}
+              <ImSun className='sun-icon' />
             </div>
           </div>
         ))}
--- src/components/SwitchButton.tsx
+++ src/components/SwitchButton.tsx
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import SwitchSelector from "react-switch-selector";
+import "./SwitchButton.css";
 
-const SimpleSwitchButton = () => {
-    const options = [
-        { label: "Option 1", value: "option1" },
-        { label: "Option 2", value: "option2" },
-    ];
-
-    const handleOptionChange = (value) => {
-        console.log("Selected option:", value);
-    };
-
-    return (
-        <SwitchSelector
-            options={options}
-            onChange={handleOptionChange}
-            initialSelectedValue="option1"
-        />
-    );
+type SwitchButtonProps = {
+  onUnitChange: (unit: string) => void;
 };
 
-export default SimpleSwitchButton;
+const SwitchButton: React.FC<SwitchButtonProps> = ({ onUnitChange }) => {
+  const options = [
+    { label: "°C", value: "celsius" },
+    { label: "°F", value: "fahrenheit" },
+  ];
+
+  const [selectedUnit, setSelectedUnit] = useState("celsius");
+
+  const handleUnitChange = (value: string) => {
+    setSelectedUnit(value);
+    onUnitChange(value);
+  };
+
+  return (
+    <div className="temp-selector">
+      <SwitchSelector
+        options={options}
+        onChange={handleUnitChange}
+        initialSelectedIndex={selectedUnit === "celsius" ? 0 : 1}
+      />
+    </div>
+  );
+};
+
+export default SwitchButton;
